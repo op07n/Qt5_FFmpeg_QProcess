9f3520de0f78c28874cde5e37634c5e80589aa98
op07n <39182105+op07n@users.noreply.github.com>
2019-09-27 08:03:54 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "dialog.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    Dialog w;
+    w.setWindowTitle("FFmpeg Dialog");
+    w.show();
+
+    return a.exec();
+}
